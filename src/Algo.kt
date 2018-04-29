d411d2a12bba9375e0b0c2be73a8d3325baeb475
Onexx <ilya7538@mail.ru>
2018-04-29 19:10:45 +0300

    v 0.3 added search option

--- src/Algo.kt
+++ src/Algo.kt
@@ -2,20 +2,39 @@ import java.util.*
 
 val scan = java.util.Scanner(System.`in`)
 
-fun min(a: Int, b: Int): Int{
-    if(a < b){
-        return a
+fun search(index: String, value:String, memory: Deque<Pair<String, String>>){
+    var result = ArrayDeque<Pair<String, String>>()
+    for(i in memory){
+        //var t: Int = min(i.first.length, index.length)
+        var pos = 0
+        for(j in i.first.chars()){
+            if(index[pos] == i.first[j]){
+                pos++
+            }
+            else{
+                pos = 0
+            }
+            if(pos == index.length - 1){
+                result.addLast(i)
+            }
+        }
+    }
+    if(result.size == 0){
+        println("Nothing found")
     }
     else{
-        return b
+        println("${result.size} matches found:")
+        for(i in result){
+            println("${i.first} - ${i.second}")
+        }
     }
 }
 
-fun search(index: String, value:String, memory: Deque<Pair<String, String>>){
+fun delete(index: String, value:String, memory: Deque<Pair<String, String>>){
     for(i in memory){
-        var t: Int = min(i.first.length, index.length)
-        for(j in i.first.chars()){
-
+        if(i.first == index || i.second == value){
+            println("Deleting pair ${i.first} - ${i.second}")
+            memory.remove(Pair(i.first, i.second))
         }
     }
 }
@@ -29,13 +48,14 @@ fun main(args: Array<String>){
             val index: String = scan.next()
             var value: String = scan.next()
             memory.addLast(Pair(index, value))
+            println("Completed")
         }
         if(inp == "Delete" || inp == "delete"){
             var index: String = scan.next()
             var value: String = scan.next()
             for(i in memory){
                 if(i.first == index || i.second == value){
-                    println("Deleting value ${i.second} with index ${i.first}")
+                    println("Deleting pair ${i.first} - ${i.second}")
                     memory.remove(Pair(i.first, i.second))
                 }
             }
