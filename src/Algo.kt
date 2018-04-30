411f22188f5ba383408bc6f2f18b42e65664ad68
Onexx <ilya7538@mail.ru>
2018-04-30 16:36:59 +0300

    v 1.0

--- src/Algo.kt
+++ src/Algo.kt
@@ -3,21 +3,39 @@ import java.util.*
 val scan = java.util.Scanner(System.`in`)
 
 fun search(memory: Vector<Pair<String, String>>){
+    if(memory.size == 0){
+        println("Memory is clear")
+        return
+    }
     val result = Vector<Pair<String, String>>()
+    val index: String = scan.next()
+    val value: String = scan.next()
     for(i in memory){
-        val index: String = scan.next()
-        val value: String = scan.next()
-        print(value)
+        //print(value)
         var pos = 0
-        for(j in i.first.chars()){
+        for(j in 0 until i.first.length){
             if(index[pos] == i.first[j]){
                 pos++
             }
             else{
                 pos = 0
             }
-            if(pos == index.length - 1){
+            if(pos == index.length){
                 result.add(i)
+                break
+            }
+        }
+        pos = 0
+        for(j in 0 until i.second.length){
+            if(value[pos] == i.second[j]){
+                pos++
+            }
+            else{
+                pos = 0
+            }
+            if(pos == index.length){
+                result.add(i)
+                break
             }
         }
     }
@@ -25,7 +43,7 @@ fun search(memory: Vector<Pair<String, String>>){
         println("Nothing found")
     }
     else{
-        println("${result.size} matches found:")
+        println("${result.size} match(es) found:")
         for(i in result){
             println("${i.first} - ${i.second}")
         }
@@ -38,49 +56,72 @@ fun delete(memory: Vector<Pair<String, String>>){
     val result: Vector<Int> = Vector()
     for(i in 0 until memory.size){
         if(memory[i].first == index || memory[i].second == value){
+            if(memory[i].first == index && memory[i].second == value){
+                println("Deleting pair ${memory[i].first} - ${memory[i].second}")
+                memory.remove(Pair(memory[i].first, memory[i].second))
+                return
+            }
             result.add(i)
         }
     }
-    when (result.size){
-        0 -> println("Nothing found")
-        1 -> { println("Deleting pair ${memory[result[0]].first} - ${memory[result[0]].second}")
-            memory.remove(Pair(memory[result[0]].first, memory[result[0]].second)) }
-        else -> {
-            println("Write number of pair, which you want to delete")
-            for(i in 0 until result.size){
-                println("${i + 1}) ${memory[result[i]].first} - ${memory[result[i]].second}")
+    if(result.size == 0){
+        println("Nothing found")
+        return
+    }
+    if(result.size == 1){
+        println("Deleting pair ${memory[result[0]].first} - ${memory[result[0]].second}")
+        memory.remove(Pair(memory[result[0]].first, memory[result[0]].second))
+        return
+    }
+    //println("Write number of pair which you want to delete")
+    println("Pick an option below to delete it")
+    for(i in 0 until result.size){
+        println("${i + 1} : ${memory[result[i]].first} - ${memory[result[i]].second}")
+    }
+    println("c : Cancel")
+    var p: Int?
+    while(true) {
+        var t: String = scan.next()
+        if(t == "C" || t == "c"){
+            println("Cancelled")
+            return
+        }
+        p = t.toIntOrNull()
+        if(p != null){
+            if(p <= result.size && p > 0){
+                break
             }
-            var p = scan.nextInt()
-            while(p > result.size || p <= 0) {
-                println("Incorrect input")
-                p = scan.nextInt()
-            }
-            println("Deleting pair ${memory[result[p-1]].first} - ${memory[result[p-1]].second}")
-            memory.remove(Pair(memory[result[p-1]].first, memory[result[p-1]].second))
+            println("Incorrect input")
+        }
+        else{
+            println("Incorrect input")
         }
     }
+    println("Deleting pair ${memory[result[p!!-1]].first} - ${memory[result[p-1]].second}")
+    memory.remove(Pair(memory[result[p-1]].first, memory[result[p-1]].second))
 }
 
 fun main(args: Array<String>){
     var inp = ""
     val memory = Vector<Pair<String, String> >()
     println("Write 'help' for help")
-    while(inp != "Exit" || inp != "exit"){
-        if(inp == "Add" || inp == "add"){
+    while(inp != "exit"){
+        if(inp == "add"){
             val index: String = scan.next()
             val value: String = scan.next()
             memory.add(Pair(index, value))
             println("Completed")
         }
-        if(inp == "Delete" || inp == "delete"){
+        if(inp == "delete"){
             delete(memory)
         }
-        if(inp == "Find" || inp == "find"){
+        if(inp == "find"){
             search(memory)
         }
-        if(inp == "Help" || inp == "help"){
+        if(inp == "help"){
             println("help option")
         }
         inp = scan.next()
+        inp = inp.toLowerCase()
     }
 }
